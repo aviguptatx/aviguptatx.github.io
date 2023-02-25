76623af138a7e2e2b447e1e381073c921829ffd5
Avinash Gupta <aviguptatx@gmail.com>
2023-02-25 02:11:54 -0600

    add time to website updates

--- app/update_website.py
+++ app/update_website.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import datetime
 
 def update_website():
     df = pd.read_json(r'data/leaderboard.json')
     
     df.to_html('index.html')
+
+    html = open("index.html", "a")
+    html.write(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
 
     return df
 
